344bd4a0de41fe7201a0bbd380b8e8035509e43f
Ernane Ferreira <ernane.junior25@gmail.com>
2024-01-05 19:47:58 -0300

    feat: add example for testing

---
+++ example.js
@@ -0,0 +1,12 @@
+const GSBNode = require('./index.js'); // require('gsb-node');
+const URLS_TO_CHECK = [
+  'http://testsafebrowsing.appspot.com/apiv4/CHROME/MALWARE/URL/',
+  'https://ernane.dev/'
+];
+
+(function main(){
+  const gsb = new GSBNode({ apiKey: 'YOUR_API_KEY_HERE' });
+  gsb.lookup(URLS_TO_CHECK).then((res) => {
+    console.log(res);
+  });
+})();
