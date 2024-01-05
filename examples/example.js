7c89f75594f1654ec6fcd8d43c0604c4ece2a5fc
Ernane Ferreira <ernane.junior25@gmail.com>
2024-01-05 20:06:33 -0300

    feat: organizing better

--- examples/example.js
+++ examples/example.js
@@ -1,4 +1,4 @@
-const GSBNode = require('./index.js'); // require('gsb-node');
+const GSBNode = require('../GSBNode.js'); // require('gsb-node');
 const URLS_TO_CHECK = [
   'http://testsafebrowsing.appspot.com/apiv4/CHROME/MALWARE/URL/',
   'https://ernane.dev/'
